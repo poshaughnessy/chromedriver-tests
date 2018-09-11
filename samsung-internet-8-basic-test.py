0ea5645d2a97b5bfac0a469b791ec1f9c474c980
Peter Oshaughnessy <peter.oshaughnessy@gmail.com>
2018-09-11 16:59:53 +0100

    Switch to beta, add new test

--- samsung-internet-8-basic-test.py
+++ samsung-internet-8-basic-test.py
@@ -1,9 +1,12 @@
 # Basic example of running Samsung Internet v8.x using ChromeDriver v2.36
+import time
 from selenium import webdriver
 
 options = webdriver.ChromeOptions()
-options.add_experimental_option('androidPackage', 'com.sec.android.app.sbrowser')
-options.add_experimental_option('androidActivity', '.SBrowserMainActivity')
+
+# NB. when v8 stable is released you can use with 'com.sec.android.app.sbrowser'
+options.add_experimental_option('androidPackage', 'com.sec.android.app.sbrowser.beta')
+options.add_experimental_option('androidActivity', 'com.sec.android.app.sbrowser.SBrowserMainActivity')
 options.add_experimental_option('androidDeviceSocket', 'Terrace_devtools_remote')
 options.add_experimental_option('androidExecName', 'Terrace')
 
@@ -12,3 +15,8 @@ driver = webdriver.Chrome('chromedriver', chrome_options=options)
 
 # Load a website
 driver.get('https://www.samsung.com/uk/')
+
+# Wait a little time to see it load
+time.sleep(5)
+
+driver.quit()
